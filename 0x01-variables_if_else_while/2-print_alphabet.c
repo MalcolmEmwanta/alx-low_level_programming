ae9f3fe63cfab378ce3bcf75917249e511a796d2
Malcolm Emwanta <123198207+MalcolmEmwanta@users.noreply.github.com>
2023-09-08 22:06:23 +0100

    new commit

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,11 +1,16 @@
 #include <stdio.h>
-
 /**
- *
  *main - Entry point
  *
- * Return; always 0
+ * Return:always returns 0
  *
  *\
-
 int main(void)
+{
+        char a;
+
+        for (a = 'a'; a <= 'z' ; a++)
+                putchar(a);
+        putchar('\n');
+        return (0);
+}
