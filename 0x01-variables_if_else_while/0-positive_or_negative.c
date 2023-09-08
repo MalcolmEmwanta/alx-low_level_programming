6e4ff40dd2aac682ed2addf1eb2c03006bbf63c6
MalcolmEmwanta <malcolmemwanta@gmail.com>
2023-09-08 12:44:30 -0700

    new commit

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,13 +1,27 @@
+#include <stdlib.h>
+#include <time.h>
 #include <stdio.h>
-/**
-  * main - Entry point
-  *
-  *Return; always 0
-  */
+/*
+ *
+ * main - Entry point
+ *
+ * Return; always 0
+ */
 int main(void)
 {
+	int n;
 
- 
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	/* if n<=o
+	 * { printf (%d negative\n
+	 * }
+	 * else, if n==0
+	 * { printf (%d 0\n
+	 * )
+	 * else
+	 * {printf (%d positive\n)
+	 *
+	 * } */
+	return (0);
 }
-
-
