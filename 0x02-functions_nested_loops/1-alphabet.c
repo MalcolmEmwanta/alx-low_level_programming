19ed0e414e45fc10feea9a611666b48dd3c6d232
MalcolmEmwanta <malcolmemwanta@gmail.com>
2023-09-12 04:14:52 -0700

    update 0-putchar.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - ALways main
+ *
+ * Retrun: always return 0;
+ *
+ */
+
+void print_alphabet(void)
+{
+	char j;
+
+	for (j = 'a'; j <= 'z'; j++)
+	{
+		_putchar(j)
+	}
+	_putchar('\n');
+}
