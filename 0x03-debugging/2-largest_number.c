40d869a33de484dbd8c9f0787584df324e876f4b
MalcolmEmwanta <malcolmemwanta@gmail.com>
2023-09-13 09:06:26 -0700

    new commit

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -9,6 +9,7 @@
  */
 
 int largest_number(int a, int b, int c)
+{
 	int largest;
 
 	if (a > b && a > c)
@@ -22,6 +23,7 @@ int largest_number(int a, int b, int c)
 	else if (b > c)
 	{
 		largest = b;
+	}
 	else
 	{
 		largest = c;
