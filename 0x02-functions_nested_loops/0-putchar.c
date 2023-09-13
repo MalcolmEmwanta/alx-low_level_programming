b674236439d294d8f4a62517549c3e5ee78826a3
MalcolmEmwanta <malcolmemwanta@gmail.com>
2023-09-13 05:59:25 -0700

    new commit

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,21 +1,22 @@
 #include "main.h"
 
 /**
- * main - Entry point
+ * main - start point
  *
- * Return: ALways returns 0
+ * Return: Always returns 0
  *
  */
+
 int main(void)
 {
-	_putchar("_");
-	_putchar("p");
-	_putchar("u");
-	_putchar("t");
-	_putchar("c");
-	_putchar("h");
-	_putchar("a");
-	_putchar("r");
-	_putchar("\n");
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 	return (0);
 }
