bd976d199def35c28213cbf6ff847401ef4cd80a
MalcolmEmwanta <malcolmemwanta@gmail.com>
2023-09-19 14:32:31 -0700

    commit 2-strlen.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * _strlen - to return the length of a string
+ *
+ *Return;return the length
+ *
+ */
+
+int _strlen(char *s)
+{
+	int i;
+
+	for (i = 0; s[1] != '\0'; i++)
+	{}
+	return (i);
+}
