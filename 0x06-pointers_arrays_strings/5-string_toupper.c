15f1ae95fcbcf99166238b1e827cc06975568d15
MalcolmEmwanta <malcolmemwanta@gmail.com>
2023-09-22 06:00:32 -0700

    new commit

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -1,1 +1,18 @@
+#include "main.h"
+/**
+ * string_toupper - lowercase to uppercase
+ * @c: character to make uppercase
+ *
+ * Return: return uppercase
+ */
 
+char *string_toupper(char *c)
+{
+	int i;
+
+	for (i = 0; c[i] != '\0'; i++)
+		if (c[i] >= 'a' && c[i] <= 'z')
+			c[i] = c[i] - 32;
+
+	return (c);
+}
