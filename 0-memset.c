a6263d67bcd2a872abe2e9aa6c86ef4a84ce0bbe
MalcolmEmwanta <malcolmemwanta@gmail.com>
2023-09-25 08:11:54 -0700

    commit memset

---
+++ 0-memset.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _memset() - fill memory  with a constant byte
+ * @b: byte to fill
+ * @n: number of bytes to fill
+ * @s: pointer to the address
+ *
+ * Return: return to *s
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+	for (i = 0; i < n; i++)
+	{
+		s[i] = b;
+	}
+	return s;
+}
