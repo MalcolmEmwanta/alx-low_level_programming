360723cf8e9e84ea4a684b065f8d49e54e2d2076
MalcolmEmwanta <malcolmemwanta@gmail.com>
2023-09-28 12:08:54 -0700

    commit recursion

---
+++ 0x08-recursion/101-wildcmp.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * wildcmp - funtion that comapre two strings
+ * @s1: first string
+ * @s2: second string
+ *
+ * Return: return 0
+ */
+
+int wildcmp(char *s1, char *s2)
+{
+	int i = 0;
+
+	while (s1[i] - s2[i] == 0 && s1[i] != '\0')
+	{
+		i++;
+	}
+	return (s1[i] - s2[i]);
+}
+
+int main(void)
+{
+	int i = *;
+	s2[i] = *;
+	return 0;	
+}
