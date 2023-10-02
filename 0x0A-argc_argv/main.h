dee90f51c45b7202122e48f2040d92a82f833640
MalcolmEmwanta <malcolmemwanta@gmail.com>
2023-10-02 13:25:18 -0700

    new commit

---
+++ 0x0A-argc_argv/main.h
@@ -0,0 +1,12 @@
+#ifndef main_h
+#define main_h
+
+int _putchar(char c);
+int main(int argc, char *argv[]);
+int main(int argc, char  *argv[]);
+int main(int argc, char  *argv[]);
+int main(int argc, char  *argv[]);
+int StringCheck(char *s);
+int main(int argc, char  *argv[]);
+
+#endif
